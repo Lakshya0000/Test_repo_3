c4c7fafcf410f581a74cb8a3fb20c4f7c137718c
Lakshya <agarwallakshya94@gmail.com>
2026-03-12 16:25:55 +0530

    init

--- solution/src/config/db.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const MONGO_URI = "mongodb://localhost:27017/dhurandhar";
-
-export async function connectDB(): Promise<void> {
-  await mongoose.connect(MONGO_URI);
-  console.log(`MongoDB connected: ${MONGO_URI}`);
-}
